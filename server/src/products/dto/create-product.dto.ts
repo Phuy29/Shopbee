7f11633e4fc11783a2d663feb308ff152c7c8a97
Phuy29 <phuchuy292003@gmail.com>
2023-09-10 16:47:40 +0700

    feat-BE: Input Validation & Transformation for product routes

--- server/src/products/dto/create-product.dto.ts
+++ server/src/products/dto/create-product.dto.ts
@@ -1,25 +1,49 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Category } from '@prisma/client';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   name: string;
 
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(300)
   @ApiProperty({ required: false })
   description?: string;
 
+  @IsNumber()
+  @IsNotEmpty()
   @ApiProperty({ default: 0 })
   price: number;
 
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   @ApiProperty({ required: false, default: null })
   images?: string = null;
 
+  @IsEnum(Category)
+  @IsNotEmpty()
   @ApiProperty()
   category: Category;
 
+  @IsNumber()
+  @IsNotEmpty()
   @ApiProperty({ default: 0 })
   inventory: number;
 
+  @IsNumber()
   @ApiProperty({ required: false, nullable: true })
   storeId: number | null;
 }
--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -28,17 +29,20 @@ export class ProductsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: string) {
     return this.productsService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  update(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: string) {
     return this.productsService.remove(+id);
   }
 }
